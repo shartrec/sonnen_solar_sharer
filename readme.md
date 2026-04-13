6d66603bcadbe3600ef8c89b23fb4fb5322b7dfb
Trevor Campbell <trevor@shartrec.com>
2026-04-14 09:05:15 +1000

    Added copyright notice

--- readme.md
+++ readme.md
@@ -181,7 +181,13 @@ cargo doc --open
 
 ## License
 
-[Add your license information here]
+This project is licensed under the GNU General Public License v2.0 - see the LICENSE file for details.
+
+**sonnen_solar_sharer** is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+
+**sonnen_solar_sharer** is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with **sonnen_solar_sharer**; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 ## Support
 
--- src/schedule.rs
+++ src/schedule.rs
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2026. Trevor Campbell and others.
+ *
+ * This file is part of sonnen_solar_sharer.
+ *
+ * sonnen_solar_sharer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,or
+ * (at your option) any later version.
+ *
+ * sonnen_solar_sharer is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with sonnen_solar_sharer; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Contributors:
+ *      Trevor Campbell
+ *
+ */
+
 use crate::config::Config;
 use chrono::{Local, NaiveTime};
 use log::{debug, error, info, warn};
